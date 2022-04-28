bc232c710a3b94e5d34476d2b886f4e2c4af7e92
oliviacowan <olivia.cowan@hotmail.com>
2022-04-28 10:48:50 -0700

    first commit

---
+++ features/arrays.ts
@@ -0,0 +1,32 @@
+// use annotation when initializing empty array
+
+const carMakers = ['Ford', 'Toyota', 'Chevy'];
+
+// type: Date[]
+const dates = [new Date(), new Date()];
+
+// type: string[][]
+const carsByMake = [
+  ['f150'],
+  ['corolla'], 
+  ['camaro']
+];
+
+// help with inference when extracting values
+const cars = carMakers[0];
+const myCars = carMakers.pop();
+
+// prevent incompatible values --
+//carMakers.push(8) will error
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+})
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+
+importantDates.push('2030-09-07');
+importantDates.push(new Date());
+// importantDates.push(8) gives error

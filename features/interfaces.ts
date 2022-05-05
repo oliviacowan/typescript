fe81d9c5020d0d7b7e177e4718053c052a10e6cf
oliviacowan <olivia.cowan@hotmail.com>
2022-05-04 17:12:16 -0700

    complete interface section

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,12 +1,41 @@
+// interface Vehicle {
+//   name: string;
+//   year: Date;
+//   broken: boolean;
+//   summary(): string;
+// }
+
+// can simplify to --->
+
+interface Reportable {
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`
+  }
 };
 
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
-
-  console.log(`Name: ${vehicle.name}`)
-  console.log(`Year: ${vehicle.year}`)
-  console.log(`Broken: ${vehicle.broken}`)
+const newDrink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `my drink has ${this.sugar} grams of sugar`
+  }
 }
+
+// const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+// rewrite with interface
+const printReport = (item: Reportable): void => {
+  // console.log(`Name: ${vehicle.name}`)
+  // console.log(`Year: ${vehicle.year}`)
+  // console.log(`Broken? ${vehicle.broken}`)
+  console.log(item.summary())
+}
+printReport(oldCivic);
+printReport(newDrink)
